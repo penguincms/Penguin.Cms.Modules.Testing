b19b43fc04058408318162a63be0e349078b737e
anloffredo <loffredo.ap@gmail.com>
2019-10-23 18:02:45 -0700

    Module Commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Penguin.Cms.Modules.Testing.Controllers
+{
+    public partial class TestController : Controller
+    {
+        public ActionResult ClearCookiesStorage() => this.View();
+
+        public ActionResult ClearLocalStorage() => this.View();
+
+        public ActionResult ClearSessionStorage() => this.View();
+
+        public ActionResult ClearStorage() => this.View();
+    }
+}
---
+++ Validators/PropertyValidator.cs
@@ -0,0 +1,50 @@
+﻿using Penguin.Testing.Interfaces;
+using Penguin.Testing.RuntimeValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Penguin.Cms.Modules.Testing.Validators
+{
+    public abstract class PropertyNameValidator : IRuntimeValidator
+    {
+        public virtual ValidationResults Validate(params Type[] toValidate)
+        {
+            ValidationResults Result = new ValidationResults();
+
+            List<string> PropertyNames = new List<string>();
+
+            foreach (Type t in toValidate)
+            {
+                PropertyNames.AddRange(t.GetProperties().Select(p => p.Name));
+                PropertyNames.AddRange(t.GetFields().Select(p => p.Name));
+            }
+
+            PropertyNames = PropertyNames.Distinct().ToList();
+
+            foreach (Type t in toValidate)
+            {
+                List<string> theseProperties = new List<string>();
+                theseProperties.AddRange(t.GetProperties().Select(p => p.Name).ToList());
+                theseProperties.AddRange(t.GetFields().Select(p => p.Name).ToList());
+
+                foreach (string PropertyName in PropertyNames)
+                {
+                    if (!theseProperties.Contains(PropertyName))
+                    {
+                        Result.AddResult(new ValidationResult()
+                        {
+                            Success = false,
+                            Checked = t,
+                            Message = $"Class {t.FullName} missing property {PropertyName}"
+                        });
+                    }
+                }
+            }
+
+            return Result;
+        }
+
+        public virtual ValidationResults Validate() => throw new NotImplementedException();
+    }
+}
